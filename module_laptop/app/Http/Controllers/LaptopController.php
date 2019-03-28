60be03c0e9bd417152b8739a4839b54131f216f7
bangnx1 <bangnx1@msb.com.vn>
2019-03-28 14:27:30 +0700

    update code

--- module_laptop/app/Http/Controllers/LaptopController.php
+++ module_laptop/app/Http/Controllers/LaptopController.php
@@ -7,8 +7,47 @@ use Illuminate\Http\Request;
 
 class LaptopController extends Controller
 {
+    /**
+     * Method show all data laptop
+     */
     public function index()
     {
         return Laptop::all();
+    }
+
+    /**
+     * Method find laptop by id
+     */
+    public function show($id)
+    {
+        return Laptop::find($id);
+    }
+
+    /**
+     * Method insert new laptop
+     */
+    public function store(Request $request)
+    {
+        return Laptop::create($request->all());
+    }
+
+    /**
+     * Method update laptop
+     */
+    public function update(Request $request, $id)
+    {
+        $article = Laptop::findOrFail($id);
+        $article->update($request->all());
+        return $article;
+    }
+
+    /**
+     * Method delete laptop
+     */
+    public function delete(Request $request, $id)
+    {
+        $article = Laptop::findOrFail($id);
+        $article->delete();
+        return $article;
     }
 }
--- module_laptop/routes/web.php
+++ module_laptop/routes/web.php
@@ -10,9 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
+// -------------------------------------------------------------------------
+// Router LAPTOP
 Route::get('laptop', 'LaptopController@index');
+Route::get('laptop/{id}', 'LaptopController@show');
