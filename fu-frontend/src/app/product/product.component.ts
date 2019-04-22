e9473a9ced70e78f8944cae2b95d61f92b481bc1
bangnx1 <bangnx1@msb.com.vn>
2019-04-22 22:18:33 +0700

    update code

--- fu-frontend/src/app/product/product.component.ts
+++ fu-frontend/src/app/product/product.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import * as FPT from "../../environments/environment";
+import { ProductService } from "../service/product.service";
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
+import { Product } from "../module/product";
 
 @Component({
   selector: 'app-product',
@@ -10,10 +14,26 @@ export class ProductComponent implements OnInit {
 
   // Create variable
   private IMAGES_SERVICE = FPT.environment.SERVICE;
+  // Variable get all list data product
+  list_Data_Product: Product[];
 
-  constructor() { }
+  constructor(private productService: ProductService,
+    private router: Router,
+    private location: Location) { }
+
+  /**
+   * Method find all product call method in product service
+  */
+  find_Product() {
+    this.productService.find_Product_Data().subscribe(
+      product => {
+        console.log(product);
+        this.list_Data_Product = product;
+      });
+  }
 
   ngOnInit() {
+    this.find_Product();
   }
 
 }
